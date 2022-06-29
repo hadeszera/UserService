6cabf38f9abf8822f9db5fd52ede15f749073ef7
Daniel Cenci Batista <daniel.batista@sellersfunding.com>
2022-06-29 20:48:07 -0300

    Adicionar arquivos de projeto.

---
+++ Context/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Entity;
+
+namespace UserService.Context
+{
+    public class UserDbContext : DbContext
+    {
+
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Controller/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Context;
+using UserService.Entity;
+
+namespace UserService.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+
+        private void PublishToMessageQueue(string integrationEvent, string eventData)
+        {
+            var factory = new ConnectionFactory();
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            var body = Encoding.UTF8.GetBytes(eventData);
+            channel.BasicPublish(exchange: "user",
+                                             routingKey: integrationEvent,
+                                             basicProperties: null,
+                                             body: body);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            var integrationEventData = JsonConvert.SerializeObject(new
+            {
+                id = user.ID,
+                newname = user.Name
+            });
+            PublishToMessageQueue("user.update", integrationEventData);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            var integrationEventData = JsonConvert.SerializeObject(new
+            {
+                id = user.ID,
+                name = user.Name
+            });
+            PublishToMessageQueue("user.add", integrationEventData);
+            return CreatedAtAction("GetUser", new { id = user.ID }, user);
+        }
+    }
+}
